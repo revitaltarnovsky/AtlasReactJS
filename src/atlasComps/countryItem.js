0b752a4b0621be75e645be490cd144d1803a1ef9
Revital Tarnovsky <revit91@gmail.com>
2020-08-03 01:17:18 +0300

    03 08 20

--- src/atlasComps/countryItem.js
+++ src/atlasComps/countryItem.js
@@ -1,4 +1,6 @@
 import React,{useState, useEffect} from 'react';
+import "./css/atlas.css"
+import { Map, Marker, TileLayer } from 'react-leaflet';
 
 function CountryItem (props){
   let [country,setCountry] = useState([])
@@ -13,14 +15,64 @@ function CountryItem (props){
     fetch(url)
     .then(resp=>resp.json())
     .then(data=>{
-      console.log(data[0])
-      setCountry(data[0])
+      console.log(data)
+      setCountry(data)
     })
     
   },[props.match])
   return(
-    <div>
-      <h2>{country.name}</h2>
+    <div >
+      {country.map(item=>{
+        return(
+        <div key={item.numericCode}>
+          <div className="row justify-content-center align-items-center">
+            <div style={{width:"25%"}}> 
+              <img className="float-left mr-2" src={item.flag} height="180"/>
+            </div>
+            <div>
+              <h2>{item.name}</h2>
+              <div>POP: {item.population}</div>
+              <div>Region: {item.region}</div>
+              <div>Languages: {item.languages[0].iso639_2}</div>
+              <div>Coin: {item.currencies.map(item=>{
+                return(
+                  <span  key={item.code} >
+                  {item.code},{item.name}
+                  </span>
+                )})}
+              </div>
+              <div>Capital: {item.capital}</div>
+            </div>
+          </div>
+          <div className="row justify-content-center align-items-center ">
+          <div className="leaflet-container float-left mr-5">
+          <Map center={[item.latlng[0],item.latlng[1]]} zoom={7}>
+            <TileLayer
+              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker
+              position={[item.latlng[0],item.latlng[1]]}
+            />
+          </Map>
+          </div>
+          <div>
+            Borders with states: {item.borders.map(item=>{
+              return(
+                <span>
+                  {item + " "}
+                </span>
+              ) 
+            })}
+          </div>
+          </div>
+        
+        </div>
+        )})}
+      
+      
+
+
     </div>
   )
 }
